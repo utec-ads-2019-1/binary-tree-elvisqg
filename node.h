898fbb961fb1bfa6a95ee74ddbe9bd652be0bd4a
elvisqg <32209698+elvisqg@users.noreply.github.com>
2019-05-08 18:53:25 -0500

    Update node.h

--- node.h
+++ node.h
@@ -13,6 +13,11 @@ class Node {
         T data;
         Node<T> *left;
         Node<T> *right;
+    
+        Node(){
+        	this->left=nullptr;
+        	this->right=nullptr;
+        }
 
     template<class>
     friend class BSTree; 
